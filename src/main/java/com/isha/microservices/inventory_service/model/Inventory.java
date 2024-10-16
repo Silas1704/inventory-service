996612be3e427b0f39cb9a8c1bec92e211d73d7b
godpiss <ishaeinstien1704@gmail.com>
2024-10-16 09:27:52 +0530

    first commit

---
+++ src/main/java/com/isha/microservices/inventory_service/model/Inventory.java
@@ -0,0 +1,23 @@
+package com.isha.microservices.inventory_service.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@Table(name = "t_inventory")
+public class Inventory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String skuCode;
+    private Integer quantity;
+
+}
---
+++ src/main/java/com/isha/microservices/inventory_service/repository/InventoryRepository.java
@@ -0,0 +1,11 @@
+package com.isha.microservices.inventory_service.repository;
+
+
+import com.isha.microservices.inventory_service.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InventoryRepository extends JpaRepository<Inventory,Long> {
+    boolean existsBySkuCodeAndQuantityIsGreaterThanEqual(String skuCode,Integer quantity);
+}
